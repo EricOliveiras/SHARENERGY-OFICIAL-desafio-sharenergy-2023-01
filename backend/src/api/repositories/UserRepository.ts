ff2f9b1bafac4a3db791e026e0d44470c2ae4426
Eric Oliveira <oiericoi@hotmail.com>
2022-12-27 19:57:08 -0300

    feat: add user repository

---
+++ backend/src/api/repositories/UserRepository.ts
@@ -0,0 +1,44 @@
+import { prismaService as db } from '../../utils/prisma';
+import { IUser } from '../../utils/interfaces/IUser';
+
+export class UserRepository {
+  async create(user: IUser) {
+    await db.user.create({
+      data: user
+    });
+  }
+
+  async read(id: string) {
+    const user = await db.user.findUnique({
+      where: {
+        id: id
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        created_at: true,
+        updated_at: true
+      }
+    });
+
+    return user;
+  }
+
+  async update(id: string, user: IUser) {
+    await db.user.update({
+      where: {
+        id: id
+      },
+      data: user
+    });
+  }
+
+  async delete(id: string) {
+    await db.user.delete({
+      where: {
+        id: id
+      }
+    });
+  }
+}
