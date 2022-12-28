cccfa125e0aecdde47cf15d7395b9bc0a937fc64
Eric Oliveira <oiericoi@hotmail.com>
2022-12-28 15:50:40 -0300

    feat: import express-async-errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
+import 'express-async-errors';
 import { app } from './app';
 import { port } from './config';
 
 app.listen(port, () => {
-  console.log(`🚀: server is running in port ${port}`);
+  console.log(`🚀: server is running in http://localhost:${port}/`);
 });
