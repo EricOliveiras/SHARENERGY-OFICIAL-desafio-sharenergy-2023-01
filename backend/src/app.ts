c560595875bd7467fc7ed1e7bb7b060e6c4c74f0
Eric Oliveira <oiericoi@hotmail.com>
2022-12-28 16:27:45 -0300

    feat: import route and error middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
 import { secret } from './config';
+import { routes } from './routes';
+import { errorMiddleware } from './middleware/errorMiddleware';
 
 export const app = express();
 
@@ -12,4 +14,7 @@ app.use(session({
 }));
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
+app.use(routes);
+app.use(errorMiddleware);
