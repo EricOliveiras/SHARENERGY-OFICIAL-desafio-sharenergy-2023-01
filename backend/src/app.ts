754aebeb8eed06395d057f067bcf0f0d3c7c485a
Eric Oliveira <oiericoi@hotmail.com>
2022-12-28 10:54:27 -0300

    feat: add session middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
+import { secret } from './config';
 
 export const app = express();
+
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.json());
 app.use(cors);
