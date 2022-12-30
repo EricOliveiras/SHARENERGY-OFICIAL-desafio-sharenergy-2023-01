14c188bba4a5b85f1845aa0e14eac92696be4062
Eric Oliveira <oiericoi@hotmail.com>
2022-12-30 14:45:04 -0300

    feat: add client controller

---
+++ backend/src/api/controllers/ClientController.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from 'express';
+import { IClient } from '../../utils/interfaces/IClient';
+import { ClientRepository } from '../repositories/ClientRepository';
+import { CreateClient, ReadClient, ReadAllClients, UpdateClient, DeleteClient } from '../services/client';
+
+const repository = new ClientRepository();
+
+export class ClientController {
+  static async create(req: Request, res: Response) {
+    const user_id = req.user?.id as string;
+    const { name, email, cpf, address, phone }: IClient = req.body;
+
+    const createClient = new CreateClient(repository);
+
+    await createClient.execute({
+      user_id,
+      name,
+      email,
+      cpf,
+      address,
+      phone
+    });
+
+    return res.sendStatus(201);
+  }
+
+  static async read(req: Request, res: Response) {
+    const user_id = req.user?.id as string;
+    const { id } = req.body;
+
+    const readClient = new ReadClient(repository);
+
+    const client = await readClient.execute(id, user_id);
+
+    return res.status(200).json(client);
+  }
+
+  static async readAll(req: Request, res: Response) {
+    const user_id = req.user?.id as string;
+
+    const readClients = new ReadAllClients(repository);
+
+    const clients = await readClients.execute(user_id);
+
+    return res.status(200).json(clients);
+  }
+
+  static async update(req: Request, res: Response) {
+    const user_id = req.user?.id as string;
+    const { id, name, email, cpf, address, phone } = req.body;
+
+    const updateClient = new UpdateClient(repository);
+
+    await updateClient.execute(id, user_id, {
+      name,
+      email,
+      cpf,
+      address,
+      phone
+    });
+
+    return res.sendStatus(200);
+  }
+
+  static async delete(req: Request, res: Response) {
+    const user_id = req.user?.id as string;
+    const { id } = req.body;
+
+    const deleteClient = new DeleteClient(repository);
+
+    await deleteClient.execute(id, user_id);
+
+    return res.sendStatus(200);
+  }
+}
