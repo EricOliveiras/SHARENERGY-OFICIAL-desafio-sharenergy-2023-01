2784a044c43f6a13a9df9c903387f4ca1c57c090
Eric Oliveira <oiericoi@hotmail.com>
2022-12-30 14:52:10 -0300

    feat: add client route in  index routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { rootRoute } from './rootRouter';
 import { userRouter } from './userRoute';
+import { clientRouter } from './clientRoute';
 
 export const routes = Router();
 
 routes.use('/user', userRouter);
+routes.use('/client', clientRouter);
 routes.use('/', rootRoute);
