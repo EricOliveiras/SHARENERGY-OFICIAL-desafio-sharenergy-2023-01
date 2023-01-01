fa6d8dd3f2a568e6fdc5a5f4d25c0619dd25f9f2
Eric Oliveira <oiericoi@hotmail.com>
2023-01-01 16:19:45 -0300

    feat: add footer component

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const Footer = () => {
+  return (
+    <>
+      <div className='footer'>
+        <h3 className='footer-title'>
+          <a href="/">Sharenergy</a>
+        </h3>
+        <p className='footer-paragraph'>Desenvolvido por  <a href='https://github.com/EricOliveiras'>Eric Oliveira</a></p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
