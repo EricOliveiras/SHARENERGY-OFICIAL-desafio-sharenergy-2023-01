f92f7a44da8cef5bb84f8e3d916fa60937d11645
Eric Oliveira <oiericoi@hotmail.com>
2023-01-04 21:05:46 -0300

    (feat | styles): add http cat page

---
+++ frontend/src/pages/httpCat/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import HeaderDashboard from '../../components/HeaderDashboard'
+
+import './style.css'
+import HttpCatImage from '../../components/HttpCatImage'
+
+const HttpCat = () => {
+  const [inputValue, setInputValue] = useState('404')
+  const [show, setShow] = useState('dontShow')
+  const [src, setSrc] = useState('')
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+  
+  const handleShowImage = () => {
+    setShow('show')
+    setSrc(`https://http.cat/${inputValue}`)
+  }
+
+  return (
+    <>
+      <HeaderDashboard />
+      <div className='container-cat'>
+        <h4 className='cat-title'>HTTP Cat</h4>
+        <p className='cat-paragraph'>Digite um HTTP Code</p>
+        <input className='cat-input' type="text" onChange={handleInputChange}/>
+        <HttpCatImage src={src} show={show}/>
+        <button className='cat-button' onClick={handleShowImage}>Enviar</button>
+      </div>
+    </>
+  )
+}
+
+export default HttpCat
