23aa3f85b53bb9a7435e8e29db3bb5f4fd3d4cce
Eric Oliveira <oiericoi@hotmail.com>
2023-01-10 10:11:10 -0300

    refactor: modify notify

--- frontend/src/notify/index.js
+++ frontend/src/notify/index.js
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const config = {
   position: "top-center",
-  autoClose: 5000,
+  autoClose: 3000,
   hideProgressBar: false,
   closeOnClick: true,
   pauseOnHover: true,
