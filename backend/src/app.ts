b304a71c677646c8e2f9cc243dad69d531d8c80d
Eric Oliveira <oiericoi@hotmail.com>
2023-01-10 17:16:31 -0300

    refactor: add express urlencoded middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,12 @@
 import express from 'express';
-import session from 'express-session';
 import cors from 'cors';
-import { secret } from './config';
 import { routes } from './routes';
 import { errorMiddleware } from './middleware/errorMiddleware';
 
 export const app = express();
 
-app.use(session({
-  secret: secret,
-  resave: false,
-  saveUninitialized: true
-}));
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(routes);
