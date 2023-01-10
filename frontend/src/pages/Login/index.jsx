70957647ef679490832dca989ab867889731135d
Eric Oliveira <oiericoi@hotmail.com>
2023-01-10 12:24:18 -0300

    refactor: modify login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Link, useNavigate } from 'react-router-dom'
-import Cookies from 'js-cookie'
 
 import { contextUser } from '../../contexts/userContexts'
 import { loginUser } from '../../validators/userValidators'
@@ -13,40 +12,61 @@ import Header from '../../components/Header'
 import './style.css'    
 
 const Login = () => {
-  const [isChecked, setIsChecked] = useState(true)
   const navigate = useNavigate()
+
+  const savedUsername = localStorage.getItem('username')
+  const savedPassword = localStorage.getItem('password')
+  const savedChecked = localStorage.getItem('checked')
+
+  const [checked, setChecked] = useState(false)
+  const [value, setValue] = useState({
+    username: '',
+    password: '',
+  })
+
+  useEffect(() => {
+    if(savedChecked === true) {
+      setValue({
+        username: savedUsername,
+        password: savedPassword
+      })
+      setChecked(true)
+    }
+  })
 
   const { register, handleSubmit, formState:{ errors } } = useForm({
     resolver: yupResolver(loginUser)
   })
 
-  const handleChange = (e) => {
-    setIsChecked(e.target.checked)
-    if (e.target.checked) return true
+  const handleChange = (event) => {
+    setValue({ [event.target.name]: event.target.value })
   }
 
-  const handleSave = (username, password) => {
-    if (isChecked) {
-      Cookies.set('username', username)
-      Cookies.set('password', password)
-    } else {
-      Cookies.remove('username')
-      Cookies.remove('password')
-    }
+  const handleChecked = (event) => {
+    setChecked(event.target.checked)
   }
 
   const onSubmit = async ({ username, password }) => {
     const response = await contextUser.login(username, password)
-    const token = response.data.id
 
-    try {
-      if (response.status === 200 && token) {
-        Cookies.set('token', token, { expires: 1 })
-        handleSave(username, password)
-        navigate('/dashboard')
+    if (!response) return
+
+    const token = response.data.token
+
+    if (response.status === 200 && token) {
+      localStorage.setItem('token', token)
+
+      if (checked === true) {
+        localStorage.setItem('username', username)
+        localStorage.setItem('password', password)
+        localStorage.setItem('checked', true)
+      } else {
+        localStorage.removeItem('username')
+        localStorage.removeItem('password')
+        localStorage.setItem('checked', false)
       }
-    } catch (error) {
-      console.log(error)
+
+      navigate('/dashboard')
     }
   }
 
@@ -57,16 +77,16 @@ const Login = () => {
         <h2 className="login-title">Entrar</h2>
         <form onSubmit={handleSubmit(onSubmit)} className='form'>
           <label className='label' htmlFor="username">username</label>
-          <input className='input' type="text" name="username" {...register('username')}/>
+          <input className='input' type="text" name="username" value={value.username} {...register('username', { onChange: handleChange } )}/>
           <p className='errors'>{errors.username?.message}</p>
 
           <label className='label' htmlFor="password">Senha</label>
-          <input className='input' type="password" name="password" {...register('password')}/>
+          <input className='input' type="password" name="password" value={value.password} {...register('password', { onChange: handleChange } )}/>
           <p className='errors'>{errors.password?.message}</p>
           
           <div className='container-check'>
-            <input className='check' type="checkbox" name="remember" checked={isChecked} onChange={handleChange}/>
-            <label className='remember' htmlFor="remember">Lembrar-me</label>
+            <input className='check' type="checkbox" name="rememberMe" checked={checked} onChange={handleChecked}/>
+            <label className='remember' htmlFor="rememberMe">Lembrar-me</label>
           </div>
           <p>Não tem conta? <Link className='register-link' to={'/register'}>Registre-se</Link> </p>
           <button className='login-button' type="submit">Entrar</button>
